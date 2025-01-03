c710aa61b26df90dd31808b15fca3d605b7b71bc
Thomas Hagström <thomas.hagstrom@chas.se>
2025-01-03 11:22:53 +0100

    Use correct localbroadcastmanager dependency on Android

--- android/src/main/java/expo/modules/thermalibexpo/ThermalibExpoModule.kt
+++ android/src/main/java/expo/modules/thermalibexpo/ThermalibExpoModule.kt
@@ -1,30 +1,46 @@
 package expo.modules.thermalibexpo
 
-import android.os.Build
-import android.os.Bundle
-import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.util.Log
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.exception.Exceptions
 import expo.modules.kotlin.exception.UnexpectedException
-import java.net.URL
 import uk.co.etiltd.thermalib.Device
-import uk.co.etiltd.thermalib.Sensor
 import uk.co.etiltd.thermalib.ThermaLib
 
 val TAG =  "ThermalibExpo"
 
+lateinit var TL : ThermaLib
+
+// the ThermaLib devices the master list contains. Filled in by executing a ThermaLib scan for devices
+var deviceList = arrayOf<Device>()
+
+fun refreshDeviceList() {
+  deviceList = TL.deviceList.toTypedArray()
+}
+
 class ThermalibExpoModule : Module() {
-  lateinit var TL : ThermaLib
+  private val context: Context
+  get() = requireNotNull(appContext.reactContext)
 
-  // It's a Bluetooth app, so FINE_LOCATION permission is required. See permissions() below
-  private val MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1
-
-  // the ThermaLib devices the master list contains. Filled in by executing a ThermaLib scan for devices
-  var devices = arrayOf<Device>()
+  
+  fun sendMessage(msg: String) {
+      Log.d(TAG, msg)
+      try {
+          sendEvent("onChange",
+              mapOf(
+                  "value" to msg
+              ))
+  
+      } catch (ex: Exception) {
+          ex.message?.let { Log.d(TAG, it) }
+      }
+  }
 
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
@@ -35,136 +51,117 @@ class ThermalibExpoModule : Module() {
     // The module will be accessible from `requireNativeModule('ThermalibExpo')` in JavaScript.
     Name(TAG)
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("initThermalib") {
-      return@AsyncFunction initThermalib()
-    }
-
     AsyncFunction("startScanning") {
       return@AsyncFunction startScanning()
     }
 
-    AsyncFunction("getDevices") {
+    Function("devices") {
       refreshDeviceList()
-      return@AsyncFunction devices;
+      if(deviceList.isNotEmpty()){
+          val result = Arguments.createArray()
+          for(dev in deviceList){
+              result.pushMap(convertDeviceToWritebleMap(dev))
+          }
+          return@Function result
+      }
+
+      return@Function null
     }
     
     OnCreate {        
-      // Log.d(TAG, "Init ThermaLib");
-      // TL = ThermaLib.instance(context);
-      
-      // sendEvent("onChange", mapOf(
-      //   "value" to "Register callbacks"
-      // ))
-
-      // Log.d(TAG, "Register callbacks");
-      // ThermaLib: Register callbacks. A more sophisticated implementation may register and deregister
-      // callbacks dynamically, according to the activity state (started/stopped, paused/resumed..)
-      // to avoid callbacks being invoked at inappropriate times. 
-      // TL.registerCallbacks(thermaLibCallbacks, "ThermalibExpo");     
+     initLib()
     }
   }
 
-  private val context: Context
-  get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+  private fun initLib() {
+      sendMessage("Init ThermaLib")
 
-  private fun initThermalib(){
-    sendEvent("onChange", mapOf(
-      "value" to "context ${context}"
-    ))
+      TL = ThermaLib.instance(context)
+      // You can alter how ThermaLib responds to a call to a method that is not applicable to the Device/Sensor for which
+      // it has been called. See the documentation for ThermaLib.UnsupportedCallHandling
+      //
+      TL.unsupportedCallHandling = ThermaLib.UnsupportedCallHandling.LOG
+      sendMessage("Supported protocols: ${TL.supportedTransports}")
+      sendMessage(
+          "Register callbacks on ${TL}"
+      )
 
-    Log.d(TAG, "Init ThermaLib");
-    sendEvent("onChange", mapOf(
-      "value" to "Init ThermaLib"
-    ))
-    TL = ThermaLib.instance(context);
-
-    Log.d(TAG, "Register callbacks");
-    sendEvent("onChange", mapOf(
-      "value" to "Register callbacks on ${TL}"
-    ))
-    TL.registerCallbacks(thermaLibCallbacks, TAG); 
+      TL.registerCallbacks(thermaLibCallbacks, TAG)
   }
 
   private fun startScanning() {
-
-    sendEvent("onChange", mapOf(
-      "value" to "Starting to scan"
-    ))
-
-    Log.d(TAG, "Start scanning");
-
-    // You can alter how ThermaLib responds to a call to a method that is not applicable to the Device/Sensor for which
-    // it has been called. See the documentation for ThermaLib.UnsupportedCallHandling
-    //
-    TL.unsupportedCallHandling = ThermaLib.UnsupportedCallHandling.LOG
-
     // ThermaLib: start scan for Bluetooth LE devices, with a 5-second timeout.
     // Completion will be dispatched via tlCallbacks
-    TL.stopScanForDevices();
-    TL.startScanForDevices(ThermaLib.Transport.BLUETOOTH_LE, 15);
+    val isConnected = TL.isServiceConnected(ThermaLib.Transport.BLUETOOTH_LE)
+    if (isConnected) {
+        sendMessage("No bluetooth!")
+        return
+    }
+
+    sendMessage(
+        "Starting to scan"
+    )
+
+    TL.stopScanForDevices()
+    TL.startScanForDevices(ThermaLib.Transport.BLUETOOTH_LE, 20)
   }
 
-  private fun refreshDeviceList() {
-    // ThermaLib: illustrates the deviceList attribute
-    devices = TL.deviceList.toTypedArray()
+  private fun convertDeviceToWritebleMap(dev: Device): WritableMap {
+    val map = Arguments.createMap()
+    map.putString("identifier", dev.identifier)
+    map.putString("deviceName", dev.deviceName)
+    map.putString("connectionState", dev.connectionState.toString())
+    map.putString("modelNumber",dev.modelNumber)
+    map.putString("manufacturerName",dev.manufacturerName)
+    map.putInt("batteryLevel",dev.batteryLevel)
+    map.putString("description", dev.description())
+    map.putString("deviceType", dev.deviceType.toString())
+    return map
+  }
+}
+
+/**
+* Illustrates: Handling of scan-time ThermaLib callbacks
+*/
+val thermaLibCallbacks = object : ThermaLib.ClientCallbacksBase() {
+  override fun onScanComplete(
+      transport: Int, scanResult: ThermaLib.ScanResult, numDevices: Int, errorMsg: String?
+  ) {
+      if (errorMsg !== null) {
+        Log.d(TAG, errorMsg)
+      }
+
+      if (scanResult == ThermaLib.ScanResult.SUCCESS) {
+        Log.d(TAG, 
+              "$numDevices found in scan"
+          )
+
+          refreshDeviceList()
+      } else {
+        Log.d(TAG, 
+              "Scan failed: ${scanResult.desc}"
+          )
+      }
   }
 
-  /**
-   * Illustrates: Handling of scan-time ThermaLib callbacks
-   */
-  public val thermaLibCallbacks = object : ThermaLib.ClientCallbacksBase() {
-    override fun onScanComplete(
-            transport: Int,
-            scanResult: ThermaLib.ScanResult,
-            numDevices: Int,
-            errorMsg: String?
-    ) {
-        if (scanResult == ThermaLib.ScanResult.SUCCESS) {
-            sendEvent("onChange", mapOf(
-              "value" to "${numDevices} found in scan"
-            ))
+  override fun onNewDevice(device: Device, timestamp: Long) {
+    Log.d(TAG, 
+          "New device found: ${device.deviceName}"
+      )
+      refreshDeviceList()
+  }
 
-            Log.d(TAG, "${numDevices} found in scan")
-            refreshDeviceList()
-        } else {
-            sendEvent("onChange", 
-              mapOf(
-              "value" to "Scan failed: ${scanResult.desc}"
-              ))
+  override fun onDeviceConnectionStateChanged(
+      device: Device, newState: Device.ConnectionState?, timestamp: Long
+  ) {
+    Log.d(TAG, 
+          "Device ${device.identifier} changed state -> ${device.connectionState}"
+      )
+  }
 
-            Log.e(TAG, "Scan failed: ${scanResult.desc}", 
-            IllegalStateException("Scan requested when already scanning"))
-        }
-    }
-
-    override fun onNewDevice(device: Device, timestamp: Long) {
-        Log.d(TAG, "New device found: ${device.deviceName}");
-        sendEvent("onChange", mapOf(
-          "value" to "New device found: ${device.deviceName}"
-        ))
-        refreshDeviceList()
-    }
-
-    override fun onDeviceConnectionStateChanged(device: Device, newState: Device.ConnectionState?, timestamp: Long) {
-        Log.d(TAG, "Device ${device.identifier} changed state -> ${device.connectionState.toString()}")
-        sendEvent("onChange", mapOf(
-          "value" to "Device ${device.identifier} changed state -> ${device.connectionState.toString()}"
-        ))
-        // instrument_list.adapter?.notifyDataSetChanged()
-    }
-
-    override fun onDeviceUpdated(device: Device, timestamp: Long) {
-        // instrument_list.adapter?.notifyDataSetChanged()
-    }
+  override fun onDeviceUpdated(device: Device, timestamp: Long) {
+    Log.d(TAG, 
+          "Device ${device.identifier} updated"
+      )
   }
 }
